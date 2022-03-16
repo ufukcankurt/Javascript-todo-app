e3589d6e9cda49e4fa08ddfbd1ea8df84532f2d2
ufukcankurt <ufuk.cnk@gmail.com>
2022-03-16 06:23:15 +0300

    Refactor code | createContent

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function load() {
   _endtodos = getLocalStorage("end_todos");
   if (Array.isArray(_endtodos)) {
     _endtodos.forEach((value, key) => {
-      createContentEnd(value["value"], value["id"]);
+      createContent(value["value"], value["id"], firstLoad = "true");
     });
   } else {
     _endtodos = [];
@@ -80,7 +80,7 @@ function removeEndTodo(item, uuid) {
 }
 
 // todo:: create frontend content and add event listener for button
-function createContent(newTodoValue, uuid = null) {
+function createContent(newTodoValue, uuid = null, firstLoad = null) {
   const newTodoItem = document.createElement("div");
   newTodoItem.classList.add("todo");
 
@@ -108,10 +108,19 @@ function createContent(newTodoValue, uuid = null) {
   newTodoDelete.innerHTML = "Delete";
 
   const newTodoChange = document.createElement("div");
-  newTodoChange.classList.add("finish-unfinish", "unfinishicon-color");
+  
 
   const newTodoChangeIcon = document.createElement("i");
-  newTodoChangeIcon.classList.add("fa", "fa-check");
+  
+
+  if(firstLoad == "true"){
+    newTodoChange.classList.add("finish-unfinish", "finishicon-color");
+    newTodoChangeIcon.classList.add("fa", "fa-question");
+  }else{
+    newTodoChange.classList.add("finish-unfinish", "unfinishicon-color");
+    newTodoChangeIcon.classList.add("fa", "fa-check");
+  }
+
 
   newTodoChange.appendChild(newTodoChangeIcon);
 
@@ -120,7 +129,13 @@ function createContent(newTodoValue, uuid = null) {
   newTodoItem.prepend(newTodoChange);
 
   newTodoItem.appendChild(newTodoAction);
-  todosListDOM.appendChild(newTodoItem);
+  
+
+  if(firstLoad == "true"){
+    endTodosListDOM.appendChild(newTodoItem);
+  }else{
+    todosListDOM.appendChild(newTodoItem);
+  }
 
   if (uuid == null) {
     uuid = uuidv4();
@@ -205,171 +220,6 @@ function createContent(newTodoValue, uuid = null) {
 
   newTodoChange.addEventListener("click", () => {
 
-    if (newTodoItem.parentElement.id == "todos") {
-      removeTodo(newTodoItem);
-      addEndTodo(newTodoItem);
-
-      _todos.forEach((value, key) => {
-        if (value["id"] === uuid) {
-          _endtodos.push({ id: value["id"], value: value["value"] });
-          _todos.splice(key, 1);
-        }
-      });
-      setLocalStorage("todos", _todos);
-      setLocalStorage("end_todos", _endtodos);
-
-      newTodoChangeIcon.classList.remove("fa-check");
-      newTodoChangeIcon.classList.add("fa-question");
-      newTodoChange.classList.remove("unfinishicon-color");
-      newTodoChange.classList.add("finishicon-color");
-      newTodoChange.style.left = "3%";
-    } else {
-      removeEndTodo(newTodoItem);
-      addTodo(newTodoItem);
-
-      _endtodos.forEach((value, key) => {
-        if (value["id"] === uuid) {
-          _todos.push({ id: value["id"], value: value["value"] });
-          _endtodos.splice(key, 1);
-        }
-      });
-
-      setLocalStorage("todos", _todos);
-      setLocalStorage("end_todos", _endtodos);
-      newTodoChangeIcon.classList.remove("fa-question");
-      newTodoChangeIcon.classList.add("fa-check");
-      newTodoChange.classList.remove("finishicon-color");
-      newTodoChange.classList.add("unfinishicon-color");
-      newTodoChange.style.left = "2%";
-    }
-  });
-}
-
-// todo:: create frontend content and add event listener for button | This for EndTodos 
-function createContentEnd(newTodoValue, uuid = null) {
-  const newTodoItem = document.createElement("div");
-  newTodoItem.classList.add("todo");
-
-  const newTodoItemContent = document.createElement("div");
-  newTodoItemContent.classList.add("content");
-
-  newTodoItem.appendChild(newTodoItemContent);
-
-  const newTodoItemInput = document.createElement("input");
-  newTodoItemInput.classList.add("text");
-  newTodoItemInput.type = "text";
-  newTodoItemInput.value = newTodoValue;
-  newTodoItemInput.setAttribute("readonly", "readonly");
-
-  newTodoItemContent.appendChild(newTodoItemInput);
-  const newTodoAction = document.createElement("div");
-  newTodoAction.classList.add("actions");
-
-  const newTodoEdit = document.createElement("button");
-  newTodoEdit.classList.add("edit");
-  newTodoEdit.innerHTML = "Edit";
-
-  const newTodoDelete = document.createElement("button");
-  newTodoDelete.classList.add("delete");
-  newTodoDelete.innerHTML = "Delete";
-
-  const newTodoChange = document.createElement("div");
-  newTodoChange.classList.add("finish-unfinish", "finishicon-color");
-
-  const newTodoChangeIcon = document.createElement("i");
-  newTodoChangeIcon.classList.add("fa", "fa-question");
-
-  newTodoChange.appendChild(newTodoChangeIcon);
-
-  newTodoAction.appendChild(newTodoEdit);
-  newTodoAction.appendChild(newTodoDelete);
-  newTodoItem.prepend(newTodoChange);
-
-  newTodoItem.appendChild(newTodoAction);
-  endTodosListDOM.appendChild(newTodoItem);
-
-  if (uuid == null) {
-    uuid = uuidv4();
-    _todos.push({ id: uuid, value: newTodoValue });
-    setLocalStorage("todos", _todos);
-  }
-
-  newTodoEdit.addEventListener("click", () => {
-    if (newTodoEdit.innerHTML.toLowerCase() == "edit") {
-      newTodoItemInput.removeAttribute("readonly");
-      newTodoItemInput.focus();
-      newTodoEdit.innerHTML = "SAVE";
-    } else {
-      newTodoItemInput.setAttribute("readonly", "readonly");
-      newTodoEdit.innerHTML = "EDIT";
-
-      // inputun sahip olduğu task-list'e göre değiştirme yapacağız.  [todos || end-todos]
-      if (
-        newTodoItemInput.parentElement.parentElement.parentElement.id == "todos"
-      ) {
-        _todos.forEach((value, key) => {
-          if (value["id"] === uuid) {
-            value.value = newTodoItemInput.value;
-          }
-        });
-        setLocalStorage("todos", _todos);
-      } else {
-        _endtodos.forEach((value, key) => {
-          if (value["id"] === uuid) {
-            value.value = newTodoItemInput.value;
-          }
-        });
-        setLocalStorage("end_todos", _endtodos);
-      }
-    }
-  });
-
-  newTodoDelete.addEventListener("click", () => {
-
-    if (newTodoItem.parentElement.id == "todos") {
-      Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          removeTodo(newTodoItem, uuid);
-          Swal.fire(
-            'Deleted!',
-            'Your task has been deleted.',
-            'success'
-          )
-        }
-      })
-      
-    } else {
-      Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          removeEndTodo(newTodoItem, uuid);
-          Swal.fire(
-            'Deleted!',
-            'Your task has been deleted.',
-            'success'
-          )
-        }
-      })
-      
-    }
-  });
-
-  newTodoChange.addEventListener("click", () => {
     if (newTodoItem.parentElement.id == "todos") {
       removeTodo(newTodoItem);
       addEndTodo(newTodoItem);
